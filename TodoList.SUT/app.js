61090781709e6735f83c5f7342042d6bc5a923e7
Jonathan <opperman.jonathan@gmail.com>
2017-09-26 20:35:44 +0200

    Added ExpressJS function

--- TodoList.SUT/app.js
+++ TodoList.SUT/app.js
@@ -7,29 +7,37 @@ const app = express();
 let todolist = [];
 
 /* The to do list and the form are displayed */
-app.get('/todo', function(req, res) {
-    res.render('todo.ejs', { todolist, clickHandler:"func1();" });
+app.get('/todo', function (req, res) {
+    res.render('todo.ejs', { todolist, clickHandler: "func1();" });
 })
 
-/* Adding an item to the to do list */
-.post('/todo/add/', urlencodedParser, function(req, res) {
-    if (req.body.newtodo != '') {
-        todolist.push(req.body.newtodo);
-    }
-    res.redirect('/todo');
-})
+    /* Adding an item to the to do list */
+    .post('/todo/add/', urlencodedParser, function (req, res) {
+        if (req.body.newtodo != '') {
+            todolist.push(req.body.newtodo);
+        }
+        res.redirect('/todo');
+    })
 
-/* Deletes an item from the to do list */
-.get('/todo/delete/:id', function(req, res) {
-    if (req.params.id != '') {
-        todolist.splice(req.params.id, 1);
-    }
-    res.redirect('/todo');
-})
+    /* Editing an item in the to do list */
+    .update('/todo/edit/', urlencodedParser, function (req, res) {
+        if (req.body.updatetodo != '') {
+            todolist.push(req.body.updatetodo);
+        }
+        res.redirect('/todo');
+    })
 
-/* Redirects to the to do list if the page requested is not found */
-.use(function(req, res, next){
-    res.redirect('/todo');
-})
+    /* Deletes an item from the to do list */
+    .get('/todo/delete/:id', function (req, res) {
+        if (req.params.id != '') {
+            todolist.splice(req.params.id, 1);
+        }
+        res.redirect('/todo');
+    })
 
-.listen(8080);
+    /* Redirects to the to do list if the page requested is not found */
+    .use(function (req, res, next) {
+        res.redirect('/todo');
+    })
+
+    .listen(8080);

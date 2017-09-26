7c69b6c76b1da187727746b901f52f276d710cb7
Jonathan <opperman.jonathan@gmail.com>
2017-09-26 19:46:09 +0200

    Initial

---
+++ TodoList.Tests/Specs/MultipleUsersPublicList.feature
@@ -0,0 +1,19 @@
+﻿Feature: MultipleUsersViewPublicList
+	In order to accomodate multiple users
+	As a single user
+	I should be able to view a shared list of items
+
+@smoketest
+Scenario: View a shared todo-list
+	Given I have opened the todo-list app
+	When The todo-list view has loaded
+	Then I should see the list of items
+
+@smoketest
+@loadtest
+Scenario: View a shared todo-list multiple users
+	Given I have opened the todo-list app with multiple users
+	When The todo-list view has loaded
+	Then I should see the list of items
+
+
---
+++ TodoList.Tests/TestSteps/MultipleUsersViewPublicList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TodoList.Tests.TestSteps
+{
+    [Binding]
+    [Scope(Tag = "smoketest")]
+    public class MultipleUsersViewPublicList
+    {
+        [Given(@"I have opened the todo-list app")]
+        public void GivenIHaveOpenedTheTodo_ListApp()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have opened the todo-list app with multiple users")]
+        public void GivenIHaveOpenedTheTodo_ListApp_Multiple_Users()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"The todo-list view has loaded")]
+        public void WhenTheTodo_ListViewHasLoaded()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see the list of items")]
+        public void ThenIShouldSeeTheListOfItems()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
